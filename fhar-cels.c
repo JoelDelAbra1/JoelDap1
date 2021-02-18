773d02f1145025bdee1e08930fe4de5603f05051
JoelDelAbra1 <78566418+JoelDelAbra1@users.noreply.github.com>
2021-02-18 14:55:27 -0600

    Update fhar-cels.c

--- fhar-cels.c
+++ fhar-cels.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
 
-int main ( )
-{
-printf("     Fharenheit to celsius\n");
-float celsius, fahr;
-float lower, upper, step ;
-lower = 0;
-upper = 300;
-step = 20;
-fahr = lower;
-while (fahr <= upper)
-{
-celsius = 5.0 * (fahr - 32) / 9.0;
-printf("\t%3.0f\t%3.1f\n", fahr, celsius);
-fahr += step;
-}
+#include <stdio.h>
+  
+int main () {
+  
+ printf ("     Fharenheit to celsius\n");
+  
+ float celsius, fahr;
+  
+ float lower=0, upper=300, step=20;
+  
+ fahr=lower;
+  
+while (fahr <= upper) {
+    
+    celsius = 5.0 * (fahr - 32) / 9.0;
+      
+    printf ("\t%3.0f\t%3.1f\n", fahr, celsius);
+      
+    fahr += step;
+    
+    }
+
 }
