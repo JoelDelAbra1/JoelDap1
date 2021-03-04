49b82886f62531d1e27e56fff668453d5e25684c
JoelDelAbra1 <78566418+JoelDelAbra1@users.noreply.github.com>
2021-03-04 14:16:47 -0600

    Create Else-if.c

---
+++ Else-if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ int main () {
+  
+int height = 300;
+int average = 177;
+  
+if (height <= average)
+  printf ("You are too short");
+  
+else if (height >= 210)
+  printf ("You are too tall");
+  
+else 
+  printf (" You can pass");
+
+}
