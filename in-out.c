a24447e26311fe4c82a52c78ac2ce59b391864f5
JoelDelAbra1 <78566418+JoelDelAbra1@users.noreply.github.com>
2021-04-02 12:59:28 -0600

    Update and rename inp-out.c to in-out.c

---
+++ in-out.c
@@ -0,0 +1,19 @@
+/*  
+--input--
+5                                                                                                                                                                                                
+hello 
+--output--
+10                                                                                                                                                                                               
+hello
+*/
+#include <stdio.h>
+int main() {
+
+	int num;
+	char a[100];
+	
+	scanf("%d",&num);
+	scanf("%s",a);
+	printf("%d", num*2);
+	printf("\n%s",a);
+}
