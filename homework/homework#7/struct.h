3112b2b8911d64816acba43e514fe96196ffc23a
JoelDelAbra1 <78566418+JoelDelAbra1@users.noreply.github.com>
2021-05-06 14:46:40 -0500

    Create struct.h

---
+++ homework/homework#7/struct.h
@@ -0,0 +1,15 @@
+#define MAX 25
+#define REGISTRAR 1
+#define SALIDA 2
+#define NUEVO 3
+#define FIN 4
+#define SHOW 5
+#define DATOS 25
+
+
+
+int* cuantos = 0;
+int salto ();
+int attendance=0; 
+int** conjunto; 
+void reg_in_out(int, int);
